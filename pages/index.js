a403efd50717281381ab23bbc5f7ea48b43c0a51
Pinto Manuel <pinto@fireworkx.co.za>
2023-02-17 05:56:34 +0200

    Chakra UI is installed and is working

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Heading, Button } from '@chakra-ui/react'
+
+ const Home = () =>  {
+  return (
+    <div>
+      <Heading size="lg">Welcome to my Next.js app!</Heading>
+      <Button colorScheme="blue">Click me</Button>
+    </div>
+  )
+}
+
+export default Home;

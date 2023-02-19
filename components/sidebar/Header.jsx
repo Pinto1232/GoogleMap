d7ba0e54c3179c23fb8ad0fab58a9e05500d17ad
Pinto Manuel <pinto@fireworkx.co.za>
2023-02-19 19:06:27 +0200

    Search bar and rating is now working

--- components/sidebar/Header.jsx
+++ components/sidebar/Header.jsx
@@ -1,9 +1,9 @@
-import { Flex, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { Flex, Input, InputGroup, InputRightElement, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
 import { Autocomplete } from '@react-google-maps/api'
 import React from 'react'
-import {BiSearch} from 'react-icons/bi'
+import { BiSearch, BiStar } from 'react-icons/bi'
 
-const Header = ({setType, setCoordinates, setRating}) => {
+const Header = ({ setType, setCoordinates, setRating }) => {
   return (
     <Flex
       position={'absolute'}
@@ -14,16 +14,86 @@ const Header = ({setType, setCoordinates, setRating}) => {
       py={2}
       zIndex={101}
     >
-    <Flex>
-      <Autocomplete>
-         <InputGroup width={'35vw'} shadow={'lg'}>
-             <InputRightElement 
-                  pointerEvents={'none'}
-                  children={BiSearch}
-             />
-         </InputGroup>
-      </Autocomplete>
-    </Flex>
+      <Flex>
+        {/* <Autocomplete 
+  >*/}
+        <InputGroup width={'35vw'} shadow='lg'>
+          <InputRightElement
+            pointerEvents={'none'}
+            children={<BiSearch color='gray' fontSize={20} />}
+          />
+
+          <Input
+            type={'text'}
+            placeholder='Search stores...'
+            variant={'filled'}
+            fontSize={18}
+            cursor={'pointer'}
+            background={'white'}
+            color={'gray.700'}
+            _hover={{ bg: 'whiteAlpha.800' }}
+            _focus={{ bg: 'whiteAlpha.800' }}
+            _placeholder={{ bg: 'transparent' }}
+          >
+
+          </Input>
+        </InputGroup>
+        {/* </Autocomplete> */}
+
+        {/* menu list*/}
+        <Flex
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+
+        </Flex>
+
+        {/* Rating */}
+        <Flex
+          alignItems={'center'}
+          justifyContent={'center'}
+          px={4}
+          py={2}
+          bg={'white'}
+          rounded={'full'}
+          ml={4}
+          shadow='lg'
+          cursor={'pointer'}
+        >
+          <Menu>
+            <BiStar fontSize={25} />
+            <MenuButton mx={2} transition='all 0.2s' borderRadius={'md'}>
+              Choose ratings
+            </MenuButton>
+
+            <MenuList>
+              <MenuItem
+                display={'flex'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                onClick={() => setRating("")}
+              >
+                <Text fontSize={20} fontWeight={500} color={'gray.700'}>
+                  All Rating
+                </Text>
+              </MenuItem>
+
+              <MenuItem
+                display={'flex'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                onClick={() => setRating("")}
+              >
+                <Text fontSize={20} fontWeight={500} color={'orange.500'}>
+                  2.0
+                </Text>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+
+
+      </Flex>
     </Flex>
   )
 }
